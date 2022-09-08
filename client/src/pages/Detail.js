4a01641c805a89b7ae0fd41e242c2e40035d89a8
Olive <olivelliott48@gmail.com>
2022-09-07 21:38:14 -0400

    Implement updating and deleting items from cart

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -23,12 +23,29 @@ function Detail() {
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const { products } = state;
+  const { products, cart } = state;
 
   const addToCart = () => {
+    const itemInCart = cart.find((cartItem) => cartItem._id === id);
+
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1,
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...currentProduct, purchaseQuantity: 1 },
+      });
+    }
+  };
+
+  const removeFromCart = () => {
     dispatch({
-      type: ADD_TO_CART,
-      product: { ...currentProduct, purchaseQuantity: 1 },
+      type: REMOVE_FROM_CART,
+      _id: currentProduct._id,
     });
   };
 
@@ -56,7 +73,12 @@ function Detail() {
           <p>
             <strong>Price:</strong>${currentProduct.price}{" "}
             <button>Add to Cart</button>
-            <button>Remove from Cart</button>
+            <button
+              disabled={!cart.find((p) => p._id === currentProduct._id)}
+              onClick={removeFromCart}
+            >
+              Remove from Cart
+            </button>
           </p>
 
           <img
